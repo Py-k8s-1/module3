0e71c26b705aa4f596539a1afff9a106d882454c
Py-k8s-1 <deepakkumarmishra98@gmail.com>
2022-12-05 15:06:36 +0530

    toolkitthunk

---
+++ reduxtoolkitwiththunk/src/components/Home/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useEffect } from "react";
+import MoviesList from "../MoviesList/MoviesList";
+import movieApi from "../../common/apis/movieApi";
+import { ApiKey } from "../../common/apis/MovieApiKey";
+import { useDispatch } from "react-redux";
+import { fetchAsyncMovies, fetchAsyncShows } from "../../feature/movies/MovieSlice";
+const Home = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchAsyncMovies());
+    dispatch(fetchAsyncShows());
+
+  }, [dispatch]);
+  return (
+    <div className="banner-logo">
+      <MoviesList />
+    </div>
+  );
+};
+
+export default Home;
